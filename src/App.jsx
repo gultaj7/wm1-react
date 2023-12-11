ea250b0534d62e61a419f13f9c48879d6f874a37
Gultaj Muradova 14125 <gultajmuradova14125@ada.edu.az>
2023-12-11 22:48:36 +0400

    create to do list elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
 export default function App() {
-  return "Hi"
+  return (
+    <>
+      <form className="item-form">
+        <div className="form">
+          <label htmlFor="item">insert new item</label>
+          <input type="text" id="item"/>
+        </div>
+        <button className="button">add</button>
+      </form>
+      <h1 className="header">To do list</h1>
+      <ul className="list">
+        <li>
+          <label htmlFor="">
+            <input type="checkbox" name="" id="" />
+            Item 1
+          </label>
+          <button className="delete">Delete</button>
+        </li>
+      </ul>
+    </>
+  )
+  
+
 }
